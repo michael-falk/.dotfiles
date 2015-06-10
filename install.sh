2b424f30e98e27c5dcd45570c55d52ddc2041b94
Michael Falk <michael.falk.94@gmail.com>
2015-06-10 19:51:29 -0400

    Major Overhaul - Used holman's bootstrap script to automate symlinks.

--- install.sh
+++ install.sh
@@ -2,18 +2,13 @@
 
 # Get current dir (so run this script from anywhere)
 
-export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
 
 # [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
-
-ln -sfv "$DOTFILES_DIR/rc/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/rc/.inputrc" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
 if [ "$(uname)" == "Darwin" -a -f "$DOTFILES_DIR/install/osx.sh" ]; then
 	source "$DOTFILES_DIR/install/osx.sh"
